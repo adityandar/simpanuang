2e1604d60d48a5e7db19f47ecc35ef658a347706
Faris Azizy <55827840+farisazizy@users.noreply.github.com>
2021-12-05 21:08:59 +0700

    commit

---
+++ kategoriMasuk.dart
@@ -0,0 +1,26 @@
+final List kategoriMasuk  = [
+    {
+      'id' : 0,
+      'title' : 'Gaji'
+    },
+    {
+      'id' : 1,
+      'title' : 'Hadiah'
+    },
+    {
+      'id' : 2,
+      'title' : 'Bonus'
+    },
+    {
+      'id' : 3,
+      'title' : 'Award'
+    },
+    {
+      'id' : 4,
+      'title' : 'Penjualan'
+    },
+    {
+      'id' : 5,
+      'title' : 'Lainnya'
+    }
+  ];
