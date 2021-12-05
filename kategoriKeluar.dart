d988fe65cf74ff0729f30c3a806d573443564cc5
Faris Azizy <55827840+farisazizy@users.noreply.github.com>
2021-12-05 20:57:15 +0700

    buat adit

---
+++ kategoriKeluar.dart
@@ -0,0 +1,66 @@
+final List kategoriKeluar = [
+    {
+      'id' : 0,
+      'title' : 'Donasi & Amal'
+    },
+    {
+      'id' : 1,
+      'title' : 'Belanja'
+    },
+    {
+      'id' : 2,
+      'title' : 'Tagihan'
+    },
+    {
+      'id' : 3,
+      'title' : 'Cicilan'
+    },
+    {
+      'id' : 4,
+      'title' : 'Makanan'
+    },
+    {
+      'id' : 5,
+      'title' : 'Kesehatan'
+    },
+    {
+      'id' : 6,
+      'title' : 'Edukasi'
+    },
+    {
+      'id' : 7,
+      'title' : 'Buku'
+    },
+    {
+      'id' : 8,
+      'title' : 'Bisnis'
+    },
+    {
+      'id' : 9,
+      'title' : 'Transportasi'
+    },
+    {
+      'id' : 10,
+      'title' : 'Teman & Pasangan'
+    },
+    {
+      'id' : 11,
+      'title' : 'Hiburan'
+    },
+    {
+      'id' : 12,
+      'title' : 'Keluarga'
+    },
+    {
+      'id' : 13,
+      'title' : 'Investasi'
+    },
+    {
+      'id' : 14,
+      'title' : 'Asuransi'
+    },
+    {
+      'id' : 16,
+      'title' : 'Lainnya'
+    },
+  ];
