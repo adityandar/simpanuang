3d5994eb88606d6c38beccf47607aabdabebaf6f
Aditya <adityandar@gmail.com>
2021-12-05 21:03:47 +0700

    update

--- lib/pages/choose_category_page.dart
+++ lib/pages/choose_category_page.dart
@@ -20,6 +20,21 @@ class _ChooseCategoryPageState extends State<ChooseCategoryPage> {
 
   final List kategoriKeluar = [
     {'id': 0, 'title': 'Donasi & Amal'},
+    {'id': 1, 'title': 'Belanja'},
+    {'id': 2, 'title': 'Tagihan'},
+    {'id': 3, 'title': 'Cicilan'},
+    {'id': 4, 'title': 'Makanan'},
+    {'id': 5, 'title': 'Kesehatan'},
+    {'id': 6, 'title': 'Edukasi'},
+    {'id': 7, 'title': 'Buku'},
+    {'id': 8, 'title': 'Bisnis'},
+    {'id': 9, 'title': 'Transportasi'},
+    {'id': 10, 'title': 'Teman & Pasangan'},
+    {'id': 11, 'title': 'Hiburan'},
+    {'id': 12, 'title': 'Keluarga'},
+    {'id': 13, 'title': 'Investasi'},
+    {'id': 14, 'title': 'Asuransi'},
+    {'id': 16, 'title': 'Lainnya'},
   ];
 
   int _currIdx = 0;
